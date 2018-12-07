5f985701b82c18406331de719752bdff1f90e222
richardnd5 <nathan.d.richard@gmail.com>
2018-12-07 13:01:03 -0700

    Initial Commit

---
+++ sStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sStories
+//
+//  Created by Nate on 12/7/18.
+//  Copyright © 2018 Nathan Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
