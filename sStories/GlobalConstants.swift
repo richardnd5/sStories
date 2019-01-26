6c82cff3499c2e9400cf1b8e71cb791057a9c620
richardnd5 <nathan.d.richard@gmail.com>
2019-01-26 00:45:18 -0700

    adding the missing files. For whatever reason. They didn't upload on last commit.

--- sStories/GlobalConstants.swift
+++ sStories/GlobalConstants.swift
@@ -1,19 +1,3 @@
-import UIKit
-
-struct Page {
-    let imageName: String
-    let storyText: [String]
-}
-
-enum MelodyType : CaseIterable {
-    case begin
-    case middle
-    case tonic
-    case dominant
-    case ending
-    case final
-}
-
 let tempo = 80.0
 let switchToCatchingMelodiesScene = 5
 let switchToArrangingScene = 9
@@ -36,23 +20,5 @@ let pages = [
     Page(imageName: "Pond", storyText: Array(storyline[20...22])),
 ]
 
-struct ScreenSize {
-    static let width = UIScreen.main.bounds.size.width
-    static let height = UIScreen.main.bounds.size.height
-    static let frame = CGRect(x: 0, y: 0, width: ScreenSize.width, height: ScreenSize.height)
-    static let maxWH = max(ScreenSize.width, ScreenSize.height)
-}
-
-struct DeviceType {
-    static let iPhone4orLess = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxWH < 568.0
-    static let iPhone5orSE   = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxWH == 568.0
-    static let iPhone678     = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxWH == 667.0
-    static let iPhone678p    = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxWH == 736.0
-    static let iPhoneX       = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxWH == 812.0
-    static let iPhoneXRMax   = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxWH == 896.0
-    static var hasNotch: Bool {
-        return iPhoneX || iPhoneXRMax
-    }
-}
 
 
