00064f4cf3067f4e245610facd3b31229162b14a
richardnd5 <nathan.d.richard@gmail.com>
2019-02-11 14:22:56 -0700

    added normalized piano melodies, edited throwback water code.

--- sStories/ViewController.swift
+++ sStories/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, SceneDelegate {
     }
     
     private var currentState = AppState.home
-    private var currentPage = 0
+    private var currentPage = 5
     private var tempStoryLine = 0
     private var pageTurnerVisible = false
     
@@ -40,7 +40,7 @@ class ViewController: UIViewController, SceneDelegate {
         super.viewDidLoad()
         
         createHomePage()
-        createBookmark()
+//        createBookmark()
         
     }
     
